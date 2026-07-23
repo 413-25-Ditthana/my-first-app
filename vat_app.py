d78d38f98e15c827c8ca687b6488cf0504182126
413-25-Ditthana <54764@yupparaj.ac.th>
2026-07-23 10:03:32 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 st.title("แอพพลิเคชั่นคำนาณราคาสินค้ารวมvat7%")
 price = st.number_input("กรอกราคาสินค้า(บาท):",value=0.0)
-st.header(f"•ภาษีมูลค่า(vat=7%):**{vat:.2f}**บาท")
-net.price = price - value
 vat = price * 0.07
+net.price = price - value
+st.header(f"•ภาษีมูลค่า(vat=7%):**{vat:.2f}**บาท")
 st.header(f"•ราคาสุทธิ:{net_price:.2f}บาท")
 st.divider()
 st.write("นาย ดิฐธนา สุระธนาจิต เลขที่ 25  ม.4/13")

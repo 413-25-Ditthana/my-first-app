50690d8dd846013e80985821fbca5d544d19adfb
413-25-Ditthana <54764@yupparaj.ac.th>
2026-09-17 09:45:36 +0700

    Update disount.py

--- disount.py
+++ disount.py
@@ -1,7 +1,8 @@
 import streamlit as st
 st.title("แอพพลิเคชั่นคำนาณราคาสินค้ารวม")
 price = st.number_input("กรอกราคาสินค้า(บาท):",value=0.0)
-discount = price /0.10
+if price >= 5000
+   discount = price / 0.10
 net_price = price - discount
 st.header(f"•ส่วนลด(10%):**{discount:.2f}**บาท")
 st.header(f"•ราคาสุทธิ:{net_price:.2f}บาท")

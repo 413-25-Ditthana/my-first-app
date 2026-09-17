de6b3f3578b1b810fa6b1fb8cd9e4669a6cc7543
413-25-Ditthana <54764@yupparaj.ac.th>
2026-09-17 09:37:22 +0700

    Update disount.py

--- disount.py
+++ disount.py
@@ -1,8 +1,8 @@
 import streamlit as st
-st.title("แอพพลิเคชั่นคำนาณราคาสินค้ารวมvat7%")
+st.title("แอพพลิเคชั่นคำนาณราคาสินค้ารวม")
 price = st.number_input("กรอกราคาสินค้า(บาท):",value=0.0)
-vat = price * 0.07
+vat = price * 0.10
 net_price = price + vat
-st.header(f"•ภาษีมูลค่า(vat=7%):**{vat:.2f}**บาท")
+st.header(f"•ส่วนลด(10%):**{vat:.2f}**บาท")
 st.header(f"•ราคาสุทธิ:{net_price:.2f}บาท")
 st.divider()
